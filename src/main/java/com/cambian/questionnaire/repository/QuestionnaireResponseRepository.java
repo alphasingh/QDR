44721c31990a8e63c1b6f279149813de7f1b65fa
Abhay Raj Singh <abhayraja4@gmail.com>
2020-12-28 16:12:33 +0530

    Add env variable with application.yml

--- src/main/java/com/cambian/questionnaire/repository/QuestionnaireResponseRepository.java
+++ src/main/java/com/cambian/questionnaire/repository/QuestionnaireResponseRepository.java
@@ -2,6 +2,8 @@ package com.cambian.questionnaire.repository;
 
 import com.cambian.questionnaire.entity.QuestionnaireResponse;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QuestionnaireResponseRepository extends MongoRepository<QuestionnaireResponse, String> {
 }
