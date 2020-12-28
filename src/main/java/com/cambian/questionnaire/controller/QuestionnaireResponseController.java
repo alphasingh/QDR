35925cb4a0a25eb3deadf59020486bad162ed528
Yash Sahu <yash.sahu@app-scoop.com>
2020-12-28 10:50:18 +0530

    add service layer

--- src/main/java/com/cambian/questionnaire/controller/QuestionnaireResponseController.java
+++ src/main/java/com/cambian/questionnaire/controller/QuestionnaireResponseController.java
@@ -3,7 +3,7 @@ package com.cambian.questionnaire.controller;
 import com.cambian.questionnaire.adapter.QuestionnaireResponseAdapter;
 import com.cambian.questionnaire.dto.QuestionnaireResponseDto;
 import com.cambian.questionnaire.entity.QuestionnaireResponse;
-import com.cambian.questionnaire.repository.QuestionnaireResponseRepository;
+import com.cambian.questionnaire.service.QuestionnaireResponseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,19 +18,19 @@ import java.util.List;
 public class QuestionnaireResponseController {
 
     @Autowired
-    private QuestionnaireResponseRepository repository;
+    private QuestionnaireResponseService service;
 
     @PostMapping("/add")
-    public ResponseEntity<QuestionnaireResponse> saveQuestionnaire(@RequestBody QuestionnaireResponseDto requestDto) {
+    public ResponseEntity<QuestionnaireResponseDto> saveQuestionnaire(@RequestBody QuestionnaireResponseDto requestDto) {
         final QuestionnaireResponse toBeCreated = QuestionnaireResponseAdapter.adaptForCreation(requestDto);
-        repository.save(toBeCreated);
-        final QuestionnaireResponseDto widgetResponse = QuestionnaireResponseAdapter.adaptToDto(toBeCreated);
-        return new ResponseEntity<>(toBeCreated, HttpStatus.CREATED);
+        final QuestionnaireResponse created = service.saveQuestionnaire(toBeCreated);
+        final QuestionnaireResponseDto questionnaireResponse = QuestionnaireResponseAdapter.adaptToDto(created);
+        return new ResponseEntity<QuestionnaireResponseDto>(questionnaireResponse, HttpStatus.CREATED);
     }
 
     @GetMapping("/getAll")
     public List<QuestionnaireResponse> getAllQuestionnaire() {
-        return repository.findAll();
+        return service.getAllQuestionnaire();
     }
 
 }
---
+++ src/main/java/com/cambian/questionnaire/service/QuestionnaireResponseService.java
@@ -0,0 +1,25 @@
+package com.cambian.questionnaire.service;
+
+import com.cambian.questionnaire.entity.QuestionnaireResponse;
+import com.cambian.questionnaire.repository.QuestionnaireResponseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionnaireResponseService {
+
+
+    @Autowired
+    private QuestionnaireResponseRepository repository;
+
+    public QuestionnaireResponse saveQuestionnaire(QuestionnaireResponse toBeCreated) {
+        repository.save(toBeCreated);
+        return toBeCreated;
+    }
+
+    public List<QuestionnaireResponse> getAllQuestionnaire() {
+        return repository.findAll();
+    }
+}
